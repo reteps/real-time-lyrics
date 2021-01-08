448a4900fd21de9710ff4ae9983f87bba83d081f
Peter Stenger <peter.a.stenger@gmail.com>
2021-01-08 18:27:35 -0500

    like 99.9% done;)

--- main.py
+++ main.py
@@ -2,23 +2,69 @@ from acrcloud.recognizer import ACRCloudRecognizer
 import sounddevice as sd
 import wave
 import scipy.io.wavfile as wavf
-from synced_lyric_sources import get_lyrics
+from lyrics import get_lyrics
 import json
 from timeit import default_timer as timer
 import time
-
+import pylrc
+import re
 RATE = 44100
-CHANNELS = 1
 CHUNK = 1024
 SECONDS = 10
-def save_frames_to_wav(filename, frames):
-    waveFile = wave.open(filename, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(frames))
-    waveFile.close()
 
+wordlist = '''fuck
+hoes
+hoe
+ass
+bitch
+bitches
+fucked
+titties
+pussy
+fuckin
+dope
+dick
+fucking
+shit
+damn
+codeine
+dickride
+motherfuckers
+motherfucking
+motherfuckin'
+shit's
+fuckin'
+motherfucker
+motherfucka
+fuckers
+niggas
+nigga
+niggas
+sex'''.split('\n')
+endings = [',','!','.',')','?']
+beginnings = ['(']
+addl_words = []
+for symbol in endings:
+    addl_words.extend([word + symbol for word in wordlist])
+wordlist.extend(addl_words)
+addl_words = []
+for symbol in beginnings:
+    addl_words.extend([word + symbol for word in wordlist])
+wordlist.extend(addl_words)
+def uncensor(word):
+    if '*' not in word:
+        return word
+    start, end = re.split('\*+', word)
+    if len(start) == len(end) == 0:
+        return word
+    capital = word.lower() != word
+    possible_words = list(filter(lambda x: len(x) == len(word) and x.startswith(start.lower()) and x.endswith(end.lower()), wordlist))
+    if len(possible_words) != 1:
+        return word
+    uncensored = possible_words[0]
+    if capital:
+        uncensored = uncensored[0].upper() + uncensored[1:]
+    return uncensored
 if __name__ == '__main__':
     config = {
         'host': 'identify-eu-west-1.acrcloud.com',
@@ -27,34 +73,50 @@ if __name__ == '__main__':
         'timeout': 10
     }
 
-    data = sd.rec(int(SECONDS * RATE), samplerate=RATE, channels=2)
-    sd.wait()
-    print('Snip Found')
-    wavf.write('out.wav', RATE, data)
-    re = ACRCloudRecognizer(config)
-    results = json.loads(re.recognize_by_file('out.wav', 0))
-    response_time = results['cost_time']
-    load_lyric_start = timer()
-    for result in results['metadata']['music']:
-        artist = result['artists'][0]['name'].split(';')[0]
-        song = result['title']
-        current_time = result['play_offset_ms'] / 1000
-        print('Possible song -', song, artist)
-        lyrics, url = get_lyrics(artist, song)
-        if lyrics != None:
-            print('Found URL:', url)
+
+    while True:
+        while True:
+            print('Recording... ',end='\r')
+            data = sd.rec(int(SECONDS * RATE), samplerate=RATE, channels=2)
+            sd.wait()
+            wavf.write('out.wav', RATE, data)
+            acr = ACRCloudRecognizer(config)
+            results = json.loads(acr.recognize_by_file('out.wav', 0))
+            if results.get('status') and results['status'].get('code') == 1001:
+                continue
             break
-    lyric_load_time = timer() - load_lyric_start
-
-    lyric_start_time = current_time + response_time + lyric_load_time
-    current_lyric_time = lyric_start_time
-    for i in range(len(lyrics) - 1):
-        lyric = lyrics[i]
-        if lyric[0] > current_lyric_time: # If ahead, sleep
-            time.sleep(lyric[0] - current_lyric_time + .01)
-            current_lyric_time += lyric[0] - current_lyric_time + .01
-            lyric = lyrics[i+1] # Print next line
-
-        m,s = divmod(int(lyrics[i+1][0]), 60)
-        ts = f'{m}:{s:02}'
-        print(ts, lyrics[i+1][1])
+        with open('logs/res_dump.json', 'w') as f:
+            json.dump(results, f, indent=2)
+        response_time = results['cost_time']
+        load_lyric_start = timer()
+        subs = None
+        for result in results['metadata']['music']:
+            artist = result['artists'][0]['name'].split(';')[0]
+            song = result['title']
+            offset_time = result['play_offset_ms'] / 1000
+            run_time = result['duration_ms'] / 1000
+            print(song, artist)
+            lyrics, engine = get_lyrics(artist, song)
+            if lyrics:
+                print(f'Found Using Engine "{engine}"')
+                subs = pylrc.parse(lyrics)
+                break
+        if subs is None:
+            input('No matches. Next song?')
+            continue
+        lyric_load_time = timer() - load_lyric_start
+        lyric_start_time = offset_time + response_time + lyric_load_time
+        current_time = lyric_start_time
+        lookahead_time = 2
+        song_ended = timer()
+        for line in subs:
+            time_to_line = line.time - current_time - lookahead_time
+            if time_to_line > 0: # Print the line
+                time.sleep(time_to_line)
+                current_time = line.time - lookahead_time
+            m,s = divmod(int(line.time), 60)
+            ts = f'{m}:{s:02}'
+            print(ts, ' '.join(uncensor(w) for w in line.text.split(' ')))
+        current_time += lookahead_time
+        if run_time > current_time:
+            time.sleep(run_time - current_time)
---
+++ more_lyrics_sources.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+UA = "Mozilla/5.0 (Maemo; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1"
+ERROR = False
+
+
+
+
+
+SOURCES = [SyairSource, MegalobizSource, RentAnAdvisorSource]
